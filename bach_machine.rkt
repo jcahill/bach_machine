9b5385bdff7fa95ea18eb49c8221113e8ffaf29a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-07-19 18:19:01 -0700

    Begin to input wheels

---
+++ bach_machine.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+(define voice-1-measure-1 '("r8 e g f e g e c" "e2. c4" "c4 g e' c" "e4 g e c"
+                            "r4 c8 d e4 c" "e8 f g f e d e c" "c2 e" "r8 c c d e e d c"
+                            "c4 c8 d e d e c"))
